bd91594a87033fc5bdd81a8a628a0bd93804b6fa
Pascal Schneider <77938819+passchn@users.noreply.github.com>
2025-10-14 14:40:42 +0200

    revert github codestyle changes

--- tests/RuntimeTest.php
+++ tests/RuntimeTest.php
@@ -4,11 +4,12 @@ namespace DevTheorem\Handlebars\Test;
 
 use DevTheorem\Handlebars\Runtime;
 use DevTheorem\Handlebars\RuntimeContext;
-use PhpOffice\PhpSpreadsheet\RichText\Run;
 use PHPUnit\Framework\TestCase;
 
-class RuntimeTest extends TestCase {
-    public function testIfVar(): void {
+class RuntimeTest extends TestCase 
+{
+    public function testIfVar(): void 
+    {
         $this->assertFalse(Runtime::ifvar(null, false));
         $this->assertFalse(Runtime::ifvar(0, false));
         $this->assertTrue(Runtime::ifvar(0, true));
@@ -24,7 +25,8 @@ class RuntimeTest extends TestCase {
         $this->assertTrue(Runtime::ifvar(self::createStringable('0'), false));
     }
 
-    public function testIsec(): void {
+    public function testIsec(): void 
+    {
         $this->assertTrue(Runtime::isec(null));
         $this->assertFalse(Runtime::isec(0));
         $this->assertTrue(Runtime::isec(false));
@@ -33,23 +35,17 @@ class RuntimeTest extends TestCase {
         $this->assertFalse(Runtime::isec(['1']));
     }
 
-    public function testWi(): void {
+    public function testWi(): void 
+    {
         $cx = new RuntimeContext();
-        $this->assertSame('', Runtime::wi($cx, false, null, new \stdClass(), function () {
-            return 'A';
-        }));
-        $this->assertSame('', Runtime::wi($cx, null, null, null, function () {
-            return 'A';
-        }));
-        $this->assertSame('{"a":"b"}', Runtime::wi($cx, ['a' => 'b'], null, ['a' => 'c'], function ($c, $i) {
-            return json_encode($i);
-        }));
-        $this->assertSame('-b=', Runtime::wi($cx, 'b', null, ['a' => 'b'], function ($c, $i) {
-            return "-$i=";
-        }));
+        $this->assertSame('', Runtime::wi($cx, false, null, new \stdClass(), function () {return 'A'; }));
+        $this->assertSame('', Runtime::wi($cx, null, null, null, function () {return 'A'; }));
+        $this->assertSame('{"a":"b"}', Runtime::wi($cx, ['a' => 'b'], null, ['a' => 'c'], function ($c, $i) {return json_encode($i); }));
+        $this->assertSame('-b=', Runtime::wi($cx, 'b', null, ['a' => 'b'], function ($c, $i) {return "-$i="; }));
     }
 
-    private static function createStringable(string $value): \Stringable {
+    private static function createStringable(string $value): \Stringable 
+    {
         return new class ($value) implements \Stringable {
             public function __construct(private string $value) {}
             public function __toString(): string {
