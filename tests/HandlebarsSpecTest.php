601f24152abad831133bb437d77e97caa4b966e0
Theodore Brown <theodorejb@outlook.com>
2026-02-20 11:52:40 -0600

    Fix block param scoping for custom block helpers
    Implement scopedId check so that pathed lookups like {{./value}} bypass helpers and block params, resolving directly from context. Previously both {{value}} and {{./value}} compiled identically, causing block params to shadow context values even for scoped paths.

--- tests/HandlebarsSpecTest.php
+++ tests/HandlebarsSpecTest.php
@@ -53,11 +53,6 @@ class HandlebarsSpecTest extends TestCase
             $spec['data'] = new \stdClass();
         }
 
-        // 4. block parameters, special case now do not support
-        if ($spec['it'] === 'should not take presedence over pathed values') {
-            $this->markTestIncomplete('Not supported case: just skip it');
-        }
-
         // 5. Not supported case: helperMissing and blockHelperMissing
         if (
             ($spec['it'] === 'if a context is not found, custom helperMissing is used')
@@ -120,6 +115,9 @@ class HandlebarsSpecTest extends TestCase
         }
 
         // FIX SPEC
+        if ($spec['it'] === 'helper block with complex lookup expression') {
+            $spec['helpers']['goodbyes']['php'] = str_replace('$options->fn();', '$options->fn([]);', $spec['helpers']['goodbyes']['php']);
+        }
         if ($spec['it'] === 'should take presednece over parent block params') {
             $spec['helpers']['goodbyes']['php'] = 'function($options) { static $value; if ($value === null) { $value = 1; } return $options->fn(["value" => "bar"], ["blockParams" => $options->blockParams === 1 ? [$value++, $value++] : null]);}';
         }
@@ -140,16 +138,12 @@ class HandlebarsSpecTest extends TestCase
             $helper = self::patchSafeString(
                 preg_replace('/function/', "function $hname", $func['php'], 1),
             );
-            $helper = str_replace('new \Handlebars\SafeString', 'new \DevTheorem\Handlebars\SafeString', $helper);
             $helper = str_replace('$options[\'data\']', '$options->data', $helper);
             $helper = str_replace('$options[\'hash\']', '$options->hash', $helper);
             $helper = str_replace('$arguments[count($arguments)-1][\'name\'];', '$arguments[count($arguments)-1]->name;', $helper);
-            if (($spec['it'] === 'helper block with complex lookup expression') && ($name === 'goodbyes')) {
-                $helper = str_replace('$options->fn();', '$options->fn([]);', $helper);
-            }
             $helpersList .= "$helper\n";
-            eval($helper);
         }
+        eval($helpersList);
 
         try {
             $partials = [];
@@ -302,6 +296,6 @@ class HandlebarsSpecTest extends TestCase
     private static function patchSafeString(string $code): string
     {
         $classname = '\\DevTheorem\\Handlebars\\SafeString';
-        return preg_replace('/ SafeString(\s*\(.*?\))?/', ' ' . $classname . '$1', $code);
+        return preg_replace('/ (\\\Handlebars\\\)?SafeString(\s*\(.*?\))?/', ' ' . $classname . '$2', $code);
     }
 }
