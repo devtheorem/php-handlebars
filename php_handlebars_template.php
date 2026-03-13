47144f291c46159e60ddddc63ffeadd516e36106
Theodore Brown <theodorejb@outlook.com>
2026-03-13 18:41:32 -0500

    Update compiled output

--- php_handlebars_template.php
+++ php_handlebars_template.php
@@ -1,5 +1,5 @@
-Compiled 1000 times in 3.662 s  |  3.662 ms/compile  |  19.6 KB code  (7 partials)
-Executed 1000 times in 2.543 s  |  2.543 ms/render   |  139.9 KB output
+Compiled 1000 times in 4.080 s  |  4.080 ms/compile  |  19.2 KB code  (7 partials)
+Executed 1000 times in 1.479 s  |  1.479 ms/render   |  139.9 KB output
 <?php
 use DevTheorem\Handlebars\Runtime as LR;
 return function (mixed $in = null, array $options = []) {
@@ -11,7 +11,7 @@ return function (mixed $in = null, array $options = []) {
 <head>
     <meta charset="UTF-8">
     <title>'.(LR::ifvar(LR::cv($in, 'pageTitle')) ? LR::encq(LR::cv($in, 'pageTitle')) : LR::encq(LR::cv($in, 'siteName')).' &mdash; Default Title').'</title>
-'.LR::hbbch($cx, 'each', [$in['stylesheets'] ?? null], [], [], $in, function($cx, $in) {return '        <link rel="stylesheet" href="'.LR::encq(LR::cv($in, 'url')).'" '.(LR::ifvar(LR::cv($in, 'media')) ? 'media="'.LR::encq(LR::cv($in, 'media')).'"' : '').'>
+'.LR::each($cx, $in['stylesheets'] ?? null, $in, function($cx, $in) {return '        <link rel="stylesheet" href="'.LR::encq(LR::cv($in, 'url')).'" '.(LR::ifvar(LR::cv($in, 'media')) ? 'media="'.LR::encq(LR::cv($in, 'media')).'"' : '').'>
 ';}, null).'</head>
 <body class="'.LR::encq(LR::cv($in, 'bodyClass')).' '.(LR::ifvar(LR::cv($in, 'user')) ? 'is-admin' : '').' '.(!LR::ifvar(LR::cv($in, 'user')) ? 'unverified' : '').'">
 
@@ -23,7 +23,7 @@ return function (mixed $in = null, array $options = []) {
         </a>
 
         <ul class="nav-links">
-'.LR::hbbch($cx, 'each', [$in['navItems'] ?? null], [], [], $in, function($cx, $in) {return LR::p($cx, 'nav-item', [[$in],[]], 0, '                ');}, null).'        </ul>
+'.LR::each($cx, $in['navItems'] ?? null, $in, function($cx, $in) {return LR::p($cx, 'nav-item', [[$in],[]], 0, '                ');}, null).'        </ul>
 
 '.(LR::ifvar(LR::cv($in, 'user')) ? '            <div class="user-menu">
                 <img src="'.LR::encq($in['user']['avatar'] ?? null).'" alt="'.LR::encq($in['user']['name'] ?? null).'" width="32" height="32">
@@ -40,7 +40,7 @@ return function (mixed $in = null, array $options = []) {
 ').'    </nav>
 </header>
 
-'.LR::hbbch($cx, 'each', [$in['alerts'] ?? null], [], [], $in, function($cx, $in) {return LR::p($cx, 'alert', [[$in],[]], 0, '    ');}, null).'
+'.LR::each($cx, $in['alerts'] ?? null, $in, function($cx, $in) {return LR::p($cx, 'alert', [[$in],[]], 0, '    ');}, null).'
 '.'<main id="main-content">
     <div class="container'.(LR::ifvar(LR::cv($in, 'fluid')) ? '-fluid' : '').'">
 
@@ -50,7 +50,7 @@ return function (mixed $in = null, array $options = []) {
                 <table class="table table-striped'.(LR::ifvar(LR::cv($in, 'hoverable')) ? ' table-hover' : '').(LR::ifvar(LR::cv($in, 'bordered')) ? ' table-bordered' : '').'">
                     <thead>
                     <tr>
-'.LR::hbbch($cx, 'each', [$in['columns'] ?? null], [], [], $in, function($cx, $in) {$sc=count($cx->depths);return '                            <th '.(LR::ifvar(LR::cv($in, 'width')) ? 'style="width:'.LR::encq(LR::cv($in, 'width')).'"' : '').' class="'.(LR::ifvar(LR::cv($in, 'sortable')) ? 'sortable' : '').(LR::ifvar(LR::hbch($cx, 'eq', [$in['currentSort']['key'] ?? null,$in['key'] ?? null], [], $in)) ? ' sort-'.LR::encq($in['currentSort']['dir'] ?? null) : '').'">
+'.LR::each($cx, $in['columns'] ?? null, $in, function($cx, $in) {$sc=count($cx->depths);return '                            <th '.(LR::ifvar(LR::cv($in, 'width')) ? 'style="width:'.LR::encq(LR::cv($in, 'width')).'"' : '').' class="'.(LR::ifvar(LR::cv($in, 'sortable')) ? 'sortable' : '').(LR::ifvar(LR::hbch($cx, 'eq', [$in['currentSort']['key'] ?? null,$in['key'] ?? null], [], $in)) ? ' sort-'.LR::encq($in['currentSort']['dir'] ?? null) : '').'">
 '.(LR::ifvar(LR::cv($in, 'sortable')) ? '                                    <a href="'.LR::encq($cx->depths[$sc-1]['sortBaseUrl'] ?? null).'?sort='.LR::encq(LR::cv($in, 'key')).'&dir='.(LR::ifvar(LR::hbch($cx, 'and', [LR::hbch($cx, 'eq', [$cx->depths[$sc-1]['currentSort']['key'] ?? null,$in['key'] ?? null], [], $in),LR::hbch($cx, 'eq', [$cx->depths[$sc-1]['currentSort']['dir'] ?? null,'asc'], [], $in)], [], $in)) ? 'desc' : 'asc').'">
                                     '.LR::encq(LR::cv($in, 'label')).'
                                     </a>
@@ -60,16 +60,16 @@ return function (mixed $in = null, array $options = []) {
                     </tr>
                     </thead>
                     <tbody>
-'.LR::hbbch($cx, 'each', [$in['items'] ?? null], [], ['item','i'], $in, function($cx, $in) {$sc=count($cx->depths);return '                        <tr class="'.(LR::ifvar($cx->blParam[0]['item']['deleted'] ?? null) ? 'deleted' : '').' '.(LR::ifvar(LR::hbch($cx, 'eq', [$cx->frame['index'] ?? null,$cx->depths[$sc-1]['selectedIndex'] ?? null], [], $in)) ? 'selected' : '').'" data-id="'.LR::encq($cx->blParam[0]['item']['id'] ?? null).'">
-'.LR::hbbch($cx, 'each', [$cx->depths[$sc-1]['columns'] ?? null], [], [], $in, function($cx, $in) {$sc=count($cx->depths);return '                                <td class="col-'.LR::encq(LR::cv($in, 'key')).(LR::ifvar($cx->blParam[0]['item']['highlighted'] ?? null) ? ' highlighted' : '').'">
-'.(LR::ifvar(LR::hbch($cx, 'eq', [$in['type'] ?? null,'boolean'], [], $in)) ? '                                        '.(LR::ifvar(LR::hbch($cx, 'lookup', [$cx->blParam[0]['item'] ?? null,$in['key'] ?? null], [], $in)) ? '<i class="icon-check text-success"></i>' : '<i class="icon-times text-muted"></i>').'
-' : (LR::ifvar(LR::hbch($cx, 'eq', [$in['type'] ?? null,'date'], [], $in)) ? '                                        <time datetime="'.LR::encq(LR::hbch($cx, 'formatDate', [LR::hbch($cx, 'lookup', [$cx->blParam[0]['item'] ?? null,$in['key'] ?? null], [], $in),'Y-m-d'], [], $in)).'">'.LR::encq(LR::hbch($cx, 'formatDate', [LR::hbch($cx, 'lookup', [$cx->blParam[0]['item'] ?? null,$in['key'] ?? null], [], $in),$in['format'] ?? null], [], $in)).'</time>
-' : (LR::ifvar(LR::hbch($cx, 'eq', [$in['type'] ?? null,'currency'], [], $in)) ? '                                        '.LR::encq(LR::hbch($cx, 'formatCurrency', [LR::hbch($cx, 'lookup', [$cx->blParam[0]['item'] ?? null,$in['key'] ?? null], [], $in),$cx->depths[$sc-1]['currency'] ?? null], [], $in)).'
-' : (LR::ifvar(LR::hbch($cx, 'eq', [$in['type'] ?? null,'link'], [], $in)) ? '                                        <a href="'.LR::encq(LR::hbch($cx, 'replace', [$in['linkTemplate'] ?? null,'{id}',$cx->blParam[0]['item']['id'] ?? null], [], $in)).'">'.LR::encq(LR::hbch($cx, 'lookup', [$cx->blParam[0]['item'] ?? null,$in['key'] ?? null], [], $in)).'</a>
-' : '                                        '.LR::encq(LR::hbch($cx, 'lookup', [$cx->blParam[0]['item'] ?? null,$in['key'] ?? null], [], $in)).'
+'.LR::each($cx, $in['items'] ?? null, $in, function($cx, $in, array $blockParams = []) {$sc=count($cx->depths);return '                        <tr class="'.(LR::ifvar($blockParams[0][0]['deleted'] ?? null) ? 'deleted' : '').' '.(LR::ifvar(LR::hbch($cx, 'eq', [$cx->frame['index'] ?? null,$cx->depths[$sc-1]['selectedIndex'] ?? null], [], $in)) ? 'selected' : '').'" data-id="'.LR::encq($blockParams[0][0]['id'] ?? null).'">
+'.LR::each($cx, $cx->depths[$sc-1]['columns'] ?? null, $in, function($cx, $in) use ($blockParams) {$sc=count($cx->depths);return '                                <td class="col-'.LR::encq(LR::cv($in, 'key')).(LR::ifvar($blockParams[0][0]['highlighted'] ?? null) ? ' highlighted' : '').'">
+'.(LR::ifvar(LR::hbch($cx, 'eq', [$in['type'] ?? null,'boolean'], [], $in)) ? '                                        '.(LR::ifvar(LR::hbch($cx, 'lookup', [$blockParams[0][0] ?? null,$in['key'] ?? null], [], $in)) ? '<i class="icon-check text-success"></i>' : '<i class="icon-times text-muted"></i>').'
+' : (LR::ifvar(LR::hbch($cx, 'eq', [$in['type'] ?? null,'date'], [], $in)) ? '                                        <time datetime="'.LR::encq(LR::hbch($cx, 'formatDate', [LR::hbch($cx, 'lookup', [$blockParams[0][0] ?? null,$in['key'] ?? null], [], $in),'Y-m-d'], [], $in)).'">'.LR::encq(LR::hbch($cx, 'formatDate', [LR::hbch($cx, 'lookup', [$blockParams[0][0] ?? null,$in['key'] ?? null], [], $in),$in['format'] ?? null], [], $in)).'</time>
+' : (LR::ifvar(LR::hbch($cx, 'eq', [$in['type'] ?? null,'currency'], [], $in)) ? '                                        '.LR::encq(LR::hbch($cx, 'formatCurrency', [LR::hbch($cx, 'lookup', [$blockParams[0][0] ?? null,$in['key'] ?? null], [], $in),$cx->depths[$sc-1]['currency'] ?? null], [], $in)).'
+' : (LR::ifvar(LR::hbch($cx, 'eq', [$in['type'] ?? null,'link'], [], $in)) ? '                                        <a href="'.LR::encq(LR::hbch($cx, 'replace', [$in['linkTemplate'] ?? null,'{id}',$blockParams[0][0]['id'] ?? null], [], $in)).'">'.LR::encq(LR::hbch($cx, 'lookup', [$blockParams[0][0] ?? null,$in['key'] ?? null], [], $in)).'</a>
+' : '                                        '.LR::encq(LR::hbch($cx, 'lookup', [$blockParams[0][0] ?? null,$in['key'] ?? null], [], $in)).'
 ')))).'                                </td>
 ';}, null).(LR::ifvar($cx->depths[$sc-1]['showActions'] ?? null) ? '                                <td class="actions">
-'.LR::hbbch($cx, 'each', [$cx->depths[$sc-1]['rowActions'] ?? null], [], [], $in, function($cx, $in) {return (!LR::ifvar(LR::hbch($cx, 'and', [$in['requiresAdmin'] ?? null,LR::hbch($cx, 'not', [$cx->blParam[0]['item']['isAdmin'] ?? null], [], $in)], [], $in)) ? '                                            <a href="'.LR::encq(LR::hbch($cx, 'replace', [$in['urlTemplate'] ?? null,'{id}',$cx->blParam[0]['item']['id'] ?? null], [], $in)).'"
+'.LR::each($cx, $cx->depths[$sc-1]['rowActions'] ?? null, $in, function($cx, $in) use ($blockParams) {return (!LR::ifvar(LR::hbch($cx, 'and', [$in['requiresAdmin'] ?? null,LR::hbch($cx, 'not', [$blockParams[0][0]['isAdmin'] ?? null], [], $in)], [], $in)) ? '                                            <a href="'.LR::encq(LR::hbch($cx, 'replace', [$in['urlTemplate'] ?? null,'{id}',$blockParams[0][0]['id'] ?? null], [], $in)).'"
                                                class="btn btn-sm btn-'.LR::encq(LR::cv($in, 'style')).'"
                                                '.(LR::ifvar(LR::cv($in, 'confirm')) ? 'data-confirm="'.LR::encq(LR::hbch($cx, 't', [$in['confirmKey'] ?? null], [], $in)).'"' : '').'
                                                title="'.LR::encq(LR::hbch($cx, 't', [$in['labelKey'] ?? null], [], $in)).'">
@@ -78,10 +78,10 @@ return function (mixed $in = null, array $options = []) {
 ' : '');}, null).'                                </td>
 ' : '').'                        </tr>
 ';}, function($cx, $in) {return '                        <tr><td colspan="'.LR::encq(LR::cv($in, 'columnCount')).'" class="empty-message">'.LR::encq(LR::hbch($cx, 't', ['table.empty'], [], $in)).'</td></tr>
-';}).'                    </tbody>
+';}, []).'                    </tbody>
 '.(LR::ifvar(LR::cv($in, 'showTotals')) ? '                        <tfoot>
                         <tr class="totals">
-'.LR::hbbch($cx, 'each', [$in['columns'] ?? null], [], [], $in, function($cx, $in) {$sc=count($cx->depths);return '                                <td>'.(LR::ifvar(LR::cv($in, 'showTotal')) ? LR::encq(LR::hbch($cx, 'formatCurrency', [LR::hbch($cx, 'lookup', [$cx->depths[$sc-1]['totals'] ?? null,$in['key'] ?? null], [], $in),$cx->depths[$sc-2]['currency'] ?? null], [], $in)) : '').'</td>
+'.LR::each($cx, $in['columns'] ?? null, $in, function($cx, $in) {$sc=count($cx->depths);return '                                <td>'.(LR::ifvar(LR::cv($in, 'showTotal')) ? LR::encq(LR::hbch($cx, 'formatCurrency', [LR::hbch($cx, 'lookup', [$cx->depths[$sc-1]['totals'] ?? null,$in['key'] ?? null], [], $in),$cx->depths[$sc-2]['currency'] ?? null], [], $in)) : '').'</td>
 ';}, null).'                            '.(LR::ifvar($cx->depths[count($cx->depths)-1]['showActions'] ?? null) ? '<td></td>' : '').'
                         </tr>
                         </tfoot>
@@ -98,24 +98,24 @@ return function (mixed $in = null, array $options = []) {
 ' : '').'            </div>
 
 ').'
-'.LR::hbbch($cx, 'each', [$in['sidePanels'] ?? null], [], [], $in, function($cx, $in) {return LR::p($cx, 'side-panel', [[$in],[]], 0, '            ');}, null).'
+'.LR::each($cx, $in['sidePanels'] ?? null, $in, function($cx, $in) {return LR::p($cx, 'side-panel', [[$in],[]], 0, '            ');}, null).'
     </div>
 </main>
 
 '.'<footer id="site-footer">
     <div class="container">
         <div class="footer-cols">
-'.LR::hbbch($cx, 'each', [$in['footerColumns'] ?? null], [], [], $in, function($cx, $in) {return LR::p($cx, 'footer-col', [[$in],[]], 0, '                ');}, null).'        </div>
+'.LR::each($cx, $in['footerColumns'] ?? null, $in, function($cx, $in) {return LR::p($cx, 'footer-col', [[$in],[]], 0, '                ');}, null).'        </div>
         <div class="footer-bottom">
             <p>'.LR::encq(LR::cv($in, 'copyright')).' '.(LR::ifvar(LR::cv($in, 'showYear')) ? LR::encq(LR::cv($in, 'currentYear')) : '').' '.LR::encq(LR::cv($in, 'siteName')).'</p>
 '.(LR::ifvar(LR::cv($in, 'social')) ? '                <ul class="social-links">
-'.LR::hbbch($cx, 'each', [$in['social'] ?? null], [], [], $in, function($cx, $in) {return '                        <li><a href="'.LR::encq(LR::cv($in, 'url')).'" aria-label="'.LR::encq(LR::cv($in, 'name')).'"><i class="icon-'.LR::encq(LR::cv($in, 'icon')).'"></i></a></li>
+'.LR::each($cx, $in['social'] ?? null, $in, function($cx, $in) {return '                        <li><a href="'.LR::encq(LR::cv($in, 'url')).'" aria-label="'.LR::encq(LR::cv($in, 'name')).'"><i class="icon-'.LR::encq(LR::cv($in, 'icon')).'"></i></a></li>
 ';}, null).'                </ul>
 ' : '').'        </div>
     </div>
 </footer>
 
-'.LR::hbbch($cx, 'each', [$in['scripts'] ?? null], [], [], $in, function($cx, $in) {return '    <script src="'.LR::encq(LR::cv($in, 'url')).'"'.(LR::ifvar(LR::cv($in, 'defer')) ? ' defer' : '').(LR::ifvar(LR::cv($in, 'async')) ? ' async' : '').(LR::ifvar(LR::cv($in, 'integrity')) ? ' integrity="'.LR::encq(LR::cv($in, 'integrity')).'" crossorigin="anonymous"' : '').'></script>
+'.LR::each($cx, $in['scripts'] ?? null, $in, function($cx, $in) {return '    <script src="'.LR::encq(LR::cv($in, 'url')).'"'.(LR::ifvar(LR::cv($in, 'defer')) ? ' defer' : '').(LR::ifvar(LR::cv($in, 'async')) ? ' async' : '').(LR::ifvar(LR::cv($in, 'integrity')) ? ' integrity="'.LR::encq(LR::cv($in, 'integrity')).'" crossorigin="anonymous"' : '').'></script>
 ';}, null).(LR::ifvar(LR::cv($in, 'inlineScript')) ? '    <script>
             '.LR::raw(LR::cv($in, 'inlineScript')).'
     </script>
