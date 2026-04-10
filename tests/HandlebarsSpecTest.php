547d603fdc8e6483b4fda09826d83d41dbda864e
Theodore Brown <theodorejb@outlook.com>
2026-04-10 17:51:00 -0500

    Improve spec test harness

--- tests/HandlebarsSpecTest.php
+++ tests/HandlebarsSpecTest.php
@@ -9,10 +9,9 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * @phpstan-type JsonSpec array{
- *     file: string, no: int, message: string|null, data: mixed,
- *     it: string, description: string, expected: string|null, helpers: array<mixed>,
- *     partials: array<mixed>, compileOptions: array<mixed>, template: string,
- *     exception: string|null, runtimeOptions: array<mixed>, number: string|null,
+ *     data: mixed, it: string, description: string, expected: string|null,
+ *     helpers: array<mixed>, partials: array<mixed>, compileOptions: array<mixed>,
+ *     template: string, exception: string|true, runtimeOptions: array<mixed>,
  * }
  */
 class HandlebarsSpecTest extends TestCase
@@ -39,7 +38,7 @@ class HandlebarsSpecTest extends TestCase
             || $spec['description'] === 'partials - compat mode'
 
             // stringParams mode was removed from Handlebars in 2015
-            || $spec['it'] === 'in string params mode,'
+            || isset($spec['compileOptions']['stringParams'])
 
             // Decorators are deprecated: https://github.com/handlebars-lang/handlebars.js/blob/master/docs/decorators-api.md
             || $spec['description'] === 'blocks - decorators'
@@ -64,7 +63,7 @@ class HandlebarsSpecTest extends TestCase
         $helpersList = '[';
         foreach ($spec['helpers'] as $name => $func) {
             if (!isset($func['php'])) {
-                $this->markTestIncomplete("No PHP helper code provided for [{$spec['file']}#{$spec['description']}]#{$spec['no']}");
+                continue;
             }
             $helper = self::patchHelperCode($func['php']);
             $helpersList .= "\n  '$name' => $helper,\n";
@@ -102,11 +101,11 @@ class HandlebarsSpecTest extends TestCase
                 partials: $stringPartials,
             ));
         } catch (\Exception $e) {
-            if (isset($spec['exception'])) {
+            if ($spec['exception']) {
                 $this->expectNotToPerformAssertions();
                 return;
             }
-            $this->fail("Compile error in {$spec['file']}#{$spec['description']}]#{$spec['no']}:{$spec['it']}\n" . $e->getMessage());
+            $this->fail("Compile error: {$e->getMessage()}");
         }
         $renderer = Handlebars::template($php);
 
@@ -127,30 +126,28 @@ class HandlebarsSpecTest extends TestCase
             }
             $result = $renderer($spec['data'], $ropt);
         } catch (\Exception $e) {
-            if (isset($spec['exception'])) {
+            if ($spec['exception']) {
                 $this->expectNotToPerformAssertions();
                 return;
             }
-            $this->fail("Rendering Error in " . self::getSpecDetails($spec, $php, $helpersList) . "\n\n{$e->getMessage()}");
+            $this->fail("Rendering error: {$e->getMessage()}\n\n" . self::getSpecDetails($php, $helpersList));
         }
 
-        if (isset($spec['exception'])) {
-            $this->fail("Should Fail: " . self::getSpecDetails($spec, $php, $helpersList) . "\n\nResult: $result");
+        if ($spec['exception']) {
+            $details = $spec['exception'] === true ? '.' : ": {$spec['exception']}\n";
+            $this->fail("Expected exception{$details}\nResult: $result\n\n" . self::getSpecDetails($php, $helpersList));
         }
 
-        $this->assertSame($spec['expected'], $result, self::getSpecDetails($spec, $php, $helpersList));
+        $this->assertSame($spec['expected'], $result, self::getSpecDetails($php, $helpersList));
     }
 
-    /**
-     * @param JsonSpec $spec
-     */
-    private static function getSpecDetails(array $spec, string $code, string $helpers): string
+    private static function getSpecDetails(string $code, string $helpers): string
     {
-        return "{$spec['file']}#{$spec['description']}]#{$spec['no']}:{$spec['it']}\nHelpers: $helpers\nPHP code:\n$code";
+        return "Helpers: $helpers\nPHP code:\n$code";
     }
 
     /**
-     * @return list<array{JsonSpec}>
+     * @return array<string, array{JsonSpec}>
      */
     public static function jsonSpecProvider(): array
     {
@@ -174,25 +171,26 @@ class HandlebarsSpecTest extends TestCase
             if ($contents === false) {
                 throw new \Exception("Failed to read JSON spec file {$file}");
             }
-            $i = 0;
             $json = json_decode($contents, true);
 
             foreach ($json as $spec) {
-                $ret[] = [[
-                    'file' => $file,
-                    'no' => ++$i,
-                    'message' => $spec['message'] ?? null,
+                $number = $spec['number'] ?? null;
+                $key = "{$spec['description']}: {$spec['it']}" . ($number !== null ? " #$number" : '');
+                if (isset($spec['message'])) {
+                    $key .= ": {$spec['message']}";
+                }
+
+                $ret[$key] = [[
                     'data' => $spec['data'] ?? null,
-                    'it' => $spec['it'] ?? '',
-                    'description' => $spec['description'] ?? '',
+                    'it' => $spec['it'],
+                    'description' => $spec['description'],
                     'expected' => $spec['expected'] ?? null,
                     'helpers' => $spec['helpers'] ?? [],
                     'partials' => $spec['partials'] ?? [],
                     'compileOptions' => $spec['compileOptions'] ?? [],
                     'template' => $spec['template'] ?? '',
-                    'exception' => $spec['exception'] ?? null,
+                    'exception' => $spec['exception'] ?? '',
                     'runtimeOptions' => $spec['runtimeOptions'] ?? [],
-                    'number' => $spec['number'] ?? null,
                 ]];
             }
         }
